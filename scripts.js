309592dd7d3000585e9bfff14021a54a9b86d592
powder-rose <glittervodka@yandex.ru>
2023-01-15 18:19:02 +0300

    nickname added

--- scripts.js
+++ scripts.js
@@ -5,23 +5,37 @@ let commentWindow = document.querySelector(".textwindow");
 let btnAvatar = document.querySelector(".btn-avatar");
 let avatar = document.querySelector(".avatar");
 let avatarImage = document.querySelector(".avatar-image");
+let nickname = document.querySelector('.nickname')
 
 let comment = "";
 let arr = [];
 
+
 document.addEventListener("DOMContentLoaded", function(event) {
-  let mess = localStorage.getItem("com");
+let mess = localStorage.getItem("comment");
+let nick = localStorage.getItem('nickname')
+
+
+if (nick != null) {
+  nickname.value = nick
+  nickname.classList.add("nickname-login");
+  }
 
   if (mess != null) {
     arr = mess.split(",");
     for (i = 0; i < arr.length; i++) {
+      let nicknameStyle = document.createElement("div")
+       nicknameStyle.classList.add('sended-message-nickname')
+       nicknameStyle.innerHTML = `${nick} :`
+       commentWindow.append(nicknameStyle)
+
       let savedMessage = document.createElement("div");
-      savedMessage.classList.add("message");
-      savedMessage.innerHTML = arr[i];
-      commentWindow.append(savedMessage);
+       savedMessage.classList.add("message");
+       savedMessage.innerHTML = arr[i];
+       commentWindow.append(savedMessage);
     }
   }
-});
+})
 
 document.addEventListener("DOMContentLoaded", function(event) {
   let avatr = localStorage.getItem("avatar");
@@ -35,17 +49,26 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 const newMessage = () => {
+  
   let message = document.createElement("div");
   message.classList.add("message");
-  commentWindow.append(message);
 
-  arr.push(textarea.value);
+    let nick = document.createElement("div")
+    nickname.classList.add("nickname-login");
+  nick.classList.add('sended-message-nickname')
+  nick.innerHTML = `${nickname.value} :`
 
-  if (localStorage.getItem("com") == null) {
-    localStorage.setItem("com", arr);
+  if (textarea.value != '') {
+    commentWindow.append(nick)
+    commentWindow.append(message)
   }
-  localStorage.setItem("com", arr);
-
+  
+  if (localStorage.getItem("comment") == null && textarea.value != '' || localStorage.getItem("comment") != null && textarea.value != '') {
+    arr.push(textarea.value);
+    localStorage.setItem("comment", arr);
+  }
+  
+  
   const checkSpam = str => {
     comment = textarea.value + comment;
     str = comment.toLowerCase();
@@ -77,6 +100,12 @@ const addAvatar = () => {
   }
 };
 
+const addNickname = () => {
+  if (localStorage.getItem("nickname") == null && nickname.value != '') {
+    localStorage.setItem("nickname", nickname.value);
+  }
+}
+
 const resetValue = () => {
   textarea.value = "";
 };
@@ -84,3 +113,6 @@ const resetValue = () => {
 btn.addEventListener("click", newMessage);
 btn.addEventListener("click", resetValue);
 btnAvatar.addEventListener("click", addAvatar);
+btn.addEventListener('click', addNickname)
+
+
